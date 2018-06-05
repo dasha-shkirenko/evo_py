66eee92c3aebc7f46cea26b3744c811720266a3b
Daria Shkirenko <daria.shkirenko@gmail.com>
2018-06-05 10:10:37 +0300

    task 1 improvement

--- homework/3/1_cubes.py
+++ homework/3/1_cubes.py
@@ -3,13 +3,12 @@ from random import randint
 i = 0
 
 while True:
-    cub_1 = randint(1, 7)
-    cub_2 = randint(1, 7)
+    cub_1 = randint(1, 6)
+    cub_2 = randint(1, 6)
 
     i += 1
 
     if cub_1 + cub_2 == 8:
         print("first cube: {}, second cube: {}, attempts: {}".format(cub_1, cub_2, i))
         break
-    else:
-        continue
+
